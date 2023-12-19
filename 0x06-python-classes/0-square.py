96fc2568b3a2fed42abc68829b4cf53b18c15a92
leonemunyao <leonemunyao@gmail.com>
2023-12-19 21:03:03 +0300

    An empty class square than defines a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+class Square:
+    """
+    This class represents a square.
+
+    It is defined by its sides, and provides a blueprint.
+
+    Attributes:
+    - None
+
+    Methods:
+    - None
+    """
+    pass
