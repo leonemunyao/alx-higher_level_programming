cab2a5c87c11fdd694a10f6c4e22cad7974db363
leonemunyao <leonemunyao@gmail.com>
2023-12-19 21:37:16 +0300

    A class that defines a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,1 +1,65 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+""" Module providing a definition of the class Square
+"""
+
+
+class Square():
+    """ Definition of a Square
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        """ Initialize 'Square'
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """ Get the size of a square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """ Set the size of a square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def position(self):
+        """ Get the position of a square
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, position):
+        """ Set the position of a square
+        """
+        if not (isinstance(position, tuple) and
+                len(position) == 2 and
+                isinstance(position[0], int) and
+                isinstance(position[1], int) and
+                position[0] >= 0 and
+                position[1] >= 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
+
+    def area(self):
+        """ Compute the area of a 'Square'
+        """
+        return self.size ** 2
+
+    def my_print(self):
+        """ Print a visual representation of a square
+        """
+        if self.size:
+            print('\n' * self.position[1], end="")
+            print('\n'.join(
+                [' ' * self.position[0] + '#' * self.size] * self.size
+            ))
+        else:
+            print()
