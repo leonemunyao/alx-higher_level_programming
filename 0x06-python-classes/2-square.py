815e92573536c7671bad913ae2946a789c764cc0
leonemunyao <leonemunyao@gmail.com>
2023-12-19 21:17:10 +0300

    A class that defines a square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""class Square that defines a square"""
+
+
+class Square:
+    """class Square that defines a square"""
+    __size = None
+
+    def __init__(self, size=0):
+        """Instantiation with optional size"""
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
