b34c90e0c68b9f5a496363b3bcf466507cd882b4
leonemunyao <leonemunyao@gmail.com>
2024-01-10 15:03:47 +0300

    A function that prints My name is <first_name> <last_name>

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a function"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints the first and last name passed to it."""
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print(f"My name is {first_name} {last_name}")
