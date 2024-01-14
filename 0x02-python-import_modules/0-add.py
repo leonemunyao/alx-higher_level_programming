d229decc32489ed12736eea45243ba27ba44cfcd
leonemunyao <leonemunyao@gmail.com>
2024-01-14 07:33:55 +0300

    Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from add_0 import add
+a = 1
+b = 2
 
-    a = 1
-    b = 2
+from add_0 import add
 
-    print("{} + {}".format(a, b, add(a, b)))
+result = add(a, b)
+
+print("{} + {} = {}".format(a, b, result))
