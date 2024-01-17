20fff1bec8fe1c4bc69b7a0598ef38681f6add8a
leonemunyao <leonemunyao@gmail.com>
2024-01-17 23:18:57 +0300

    Display #!

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -70,8 +70,11 @@ class Rectangle(Base):
     
     def display(self):
         """Display #0"""
-        for i in range(self.height):
-            print("#"*self.width)
+        for _ in range(self.y):
+            print()
+
+        for _ in range(self.height):
+            print(" " * self.x + "#"*self.width)
 
     def __str__(self):
         """Override the __str__ method"""
