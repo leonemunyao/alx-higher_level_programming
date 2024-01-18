fc7399611007fecef613f7f3ab2e5e866c743096
leonemunyao <leonemunyao@gmail.com>
2024-01-18 14:02:15 +0300

    Dictionary to instance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,7 +14,7 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Converts list of dictionaries into json string"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
@@ -32,3 +32,24 @@ class Base:
         with open(filename, 'w') as file:
             json_string = cls.to_json_string([obj.__dict__ for obj in list_objs])
             file.write(json_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Reads a JSON string and returns a list of objects"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        else:
+            return json.loads(json_string)
+        
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes set"""
+        if cls.__name__ == "Rectangle":
+            dummy_instance = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy_instance = cls()
+        else:
+            raise ValueError("Unsupported class")
+        
+        dummy_instance.update(**dictionary)
+        return dummy_instance
