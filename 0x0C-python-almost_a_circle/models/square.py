f678aebe54c2136bce8391daf9a43dcd071cfd9b
leonemunyao <leonemunyao@gmail.com>
2024-01-18 10:22:00 +0300

    Square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -44,3 +44,18 @@ class Square(Rectangle):
         """Setter for height"""
         super(Square, Square).width.fset(self, value)
         super(Square, Square).height.fset(self, value)
+
+    def update(self, *args, **kwargs):
+        """Assings attribues"""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
