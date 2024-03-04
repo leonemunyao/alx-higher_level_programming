375cacb5e9b87334fc105a4915afcab4992e4b48
leonemunyao <leonemunyao@gmail.com>
2024-03-04 15:09:20 +0300

    Task 7. Script that inserts a new row in the table first_table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- Script that inserts a new row in the table first_table
-INSERT INTO TABLE first_table(id, name) VALUES (89 ,Best School);
+INSERT INTO TABLE first_table(id, name) VALUES (89, "Best School");
