94b71c31ef7b6bec8b48a897912b385766a5522a
leonemunyao <leonemunyao@gmail.com>
2024-03-12 23:53:19 +0300

    A Script that prints three lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
+console.log(firstLine); // Output: C is fun
+console.log(secondLine); // Output: Python is cool
+console.log(thirdLine);
