a0811349314ffb5e25bf8e1f34f23b02c2983588
leonemunyao <leonemunyao@gmail.com>
2024-04-09 17:21:27 +0300

    Updated this script by adding a new function incr that increments the integer value

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -3,19 +3,21 @@
 /**
  Update this script by adding a new function incr that increments the integer value
  */
+
  const myObject = {
     type: 'object',
-    value: 12,
-    incr: function () {
-        this.value++;
-    }
-};
-
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-
+    value: 12
+  };
+  
+  function addIncr(obj) {
+    obj.incr = function() {
+      this.value++;
+      console.log(this);
+    };
+  }
+  
+  console.log(myObject);
+  addIncr(myObject);
+  myObject.incr();
+  myObject.incr();
+  myObject.incr();
