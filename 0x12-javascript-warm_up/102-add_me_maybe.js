4197bc5bca6ca56d5d1d6694dd66850b399db317
leonemunyao <leonemunyao@gmail.com>
2024-04-09 16:48:31 +0300

    A function that increments and calls a function

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /*
-A function that increments and calls a function. The function must be visible from outside. No var allowed.
+A function that increments and calls a function. The function must be visible from outside.
 prototype: function (number, theFunction)
 */
 exports.addMeMaybe = function (number, theFunction) {
