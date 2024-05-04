9f280dd2f2413b5fe3af53a85ec5092b6146d1c2
leonemunyao <leonemunyao@gmail.com>
2024-05-04 21:18:15 +0300

    A script that lists all records of the table second_table of the database

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- A script that lists all records of the table second_table from the database hbtn_0c_0
+SELECT * FROM second_table;
+-- A script that displays the score of the record with the highest score in the table second_table of the database hbtn_0c_0
+SELECT MAX(score) FROM second_table;
