d2907eda1c7fb5757bd79db5bf002410299fbc3c
leonemunyao <leonemunyao@gmail.com>
2024-05-04 21:57:02 +0300

    A Script that lists all records of the table second_table

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- A Script that lists all the records of the table second_table of the database hbtn_0c_0. No listing rows without the name value. Result should be listed in descending score.
-SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;"
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
