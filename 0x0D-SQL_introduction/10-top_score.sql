403f4b3c5ed171d3a2f6ffb5a3681f2fe7cfc9ff
leonemunyao <leonemunyao@gmail.com>
2024-05-04 21:22:54 +0300

    A script that lists all records of the table second_table of the database

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
--- A script that lists all records of the table second_table from the database hbtn_0c_0
-SELECT * MAX(score) FROM second_table;
+-- A script that lists all records of the table second_table from the database hbtn_0c_0. Reseult should display both score and name columns. Redocrd should be ordered by score in descending order.
+SELECT score, name FROM second_table ORDER BY score DESC;
