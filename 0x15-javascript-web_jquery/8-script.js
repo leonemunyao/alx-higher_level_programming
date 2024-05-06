cf51b54495e44c4c4b280d2cb4db0fedddd09db2
leonemunyao <leonemunyao@gmail.com>
2024-05-07 01:12:17 +0300

    Script that fetches and lists the title for all movies by using this URL: https://swapi-api.alx-tools.com/api/films/?format=json. No using document.querySelector

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,10 @@
+// JavaScript script that fetches and lists the title for all movies by using this URL: https://swapi-api.alx-tools.com/api/films/?format=json.
+// All movie titles must be list in the HTML tag UL#list_movies.
+// You can't use document.querySelector to select the HTML tag.
+// You must use the jQuery API
+
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+    for (const movie of data.results) {
+        $('UL#list_movies').append('<li>' + movie.title + '</li>');
+    }
+});
