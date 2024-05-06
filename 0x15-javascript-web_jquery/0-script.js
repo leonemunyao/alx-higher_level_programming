6a3e2eed7702e26f09c01104fdfe5f8e09ff06bb
leonemunyao <leonemunyao@gmail.com>
2024-05-06 23:22:15 +0300

    Script that updates the text color of header to red

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,2 @@
+// A Script that updates the text color of the HTML tag HEADER to red (#FF0000): You must use document.querySelector to select the HTML tag
+document.querySelector('header').style.color = '#FF0000';
