f4b7468eed767a5901ed36e3d4a3adcc72fdd7bd
leonemunyao <leonemunyao@gmail.com>
2024-05-07 01:27:59 +0300

    Script that updates the text color of the <header> element to red (#FF0000). No using document.querySelector

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+// JavaScript script that updates the text color of the <header> element to red (#FF0000) in file 100-main.html.
+// You must use the jQuery API
+// You must not use document.querySelector to select the HTML tag.
+// Script must be imported from the head tag, not at the end of the HTML.
+$('header').css('color', '#FF0000');
