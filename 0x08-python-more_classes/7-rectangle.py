9a29893b8d3cd7c0600814819baeaa007f8edf9b
leonemunyao <leonemunyao@gmail.com>
2024-05-09 13:29:50 +0300

    Added a class atrribute print_symbol

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -57,7 +57,8 @@ class Rectangle:
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join(str(self.print_symbol) * self.width for _ in range(self.height))
+        return "\n".join(str(self.print_symbol) * self.width
+                         for _ in range(self.height))
 
     def __repr__(self):
         return "Rectangle({}, {})".format(self.__width, self.__height)
